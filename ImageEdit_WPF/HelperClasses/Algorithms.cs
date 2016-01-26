84c9d1afb509c743d5a443d0013d470139138bda
nlabiris <nick.f1@hotmail.com>
2016-01-26 23:36:02 +0200

    2 changes
    - Add unsafe context to Negative algorithm. More to go.
    - Add UselessCode.cs file that contains previous implementations of algorithms.

--- ImageEdit_WPF/HelperClasses/Algorithms.cs
+++ ImageEdit_WPF/HelperClasses/Algorithms.cs
@@ -362,48 +362,43 @@ namespace ImageEdit_WPF.HelperClasses {
         #endregion
 
         #region Negative
-        public static TimeSpan Negative(ImageData data, BackgroundWorker backgroundWorker) {
-            int i = 0;
-            int j = 0;
-            int index = 0;
+        public static TimeSpan Negative(ImageData data, BackgroundWorker backgroundWorker)
+        {
+            #region unsafe
+            unsafe {
+                int i = 0;
+                int j = 0;
+                int index = 0;
 
-            // Lock the bitmap's bits.  
-            BitmapData bmpData = data.M_bitmap.LockBits(new Rectangle(0, 0, data.M_width, data.M_height), ImageLockMode.ReadWrite, data.M_bitmap.PixelFormat);
+                // Lock the bitmap's bits.  
+                BitmapData bmpData = data.M_bitmap.LockBits(new Rectangle(0, 0, data.M_width, data.M_height), ImageLockMode.ReadWrite, data.M_bitmap.PixelFormat);
 
-            // Get the address of the first line.
-            IntPtr ptr = bmpData.Scan0;
+                // Get the address of the first line.
+                byte* ptr = (byte*)bmpData.Scan0;
 
-            // Declare an array to hold the bytes of the bitmap.
-            int bytes = Math.Abs(bmpData.Stride)*data.M_bitmap.Height;
-            byte[] rgbValues = new byte[bytes];
+                Stopwatch watch = Stopwatch.StartNew();
 
-            // Copy the RGB values into the array.
-            Marshal.Copy(ptr, rgbValues, 0, bytes);
-
-            Stopwatch watch = Stopwatch.StartNew();
-
-            #region Algorithm
-            for (i = 0; i < data.M_width; i++) {
-                backgroundWorker.ReportProgress(Convert.ToInt32(((double)i/data.M_width)*100));
-                for (j = 0; j < data.M_height; j++) {
-                    index = (j*bmpData.Stride) + (i*3);
-                    rgbValues[index + 2] = (byte)(255 - rgbValues[index + 2]); // R
-                    rgbValues[index + 1] = (byte)(255 - rgbValues[index + 1]); // G
-                    rgbValues[index] = (byte)(255 - rgbValues[index]); // B
+                #region Algorithm
+                for (i = 0; i < data.M_width; i++) {
+                    backgroundWorker.ReportProgress(Convert.ToInt32(((double)i/data.M_width)*100));
+                    for (j = 0; j < data.M_height; j++) {
+                        index = (j*bmpData.Stride) + (i*3);
+                        ptr[index + 2] = (byte)(255 - ptr[index + 2]); // R
+                        ptr[index + 1] = (byte)(255 - ptr[index + 1]); // G
+                        ptr[index] = (byte)(255 - ptr[index]); // B
+                    }
                 }
+                #endregion
+
+                watch.Stop();
+                TimeSpan elapsedTime = watch.Elapsed;
+
+                // Unlock the bits.
+                data.M_bitmap.UnlockBits(bmpData);
+
+                return elapsedTime;
             }
             #endregion
-
-            watch.Stop();
-            TimeSpan elapsedTime = watch.Elapsed;
-
-            // Copy the RGB values back to the bitmap
-            Marshal.Copy(rgbValues, 0, ptr, bytes);
-
-            // Unlock the bits.
-            data.M_bitmap.UnlockBits(bmpData);
-
-            return elapsedTime;
         }
         #endregion
 
